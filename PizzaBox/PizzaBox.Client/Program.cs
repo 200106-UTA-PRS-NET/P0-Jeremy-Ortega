f9589e094f31b8126d578568c8e830a61edad554
Jeremy Ortega <jpojeremyortega@live.com>
2020-01-12 19:22:01 -0600

    test push

---
+++ PizzaBox/PizzaBox.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PizzaBox.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignIn();
+        }
+
+
+
+        private static void SignIn()
+        {
+            
+        }
+    }
+
+
+    
+
+}
---
+++ PizzaBox/PizzaBox.Domain/Models/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+    
+
+
+namespace PizzaBox.Domain.Models
+{
+
+    // public class Pizza : PizzaBox.Domain.Interfaces.IPizza
+    public class Pizza
+    {
+
+    }
+}
