c7538a6d5bb8013bc5fc1c967e37844c4e67f55c
Jeremy Ortega <jpojeremyortega@live.com>
2020-01-12 20:37:50 -0600

    moved some stuff around and made initial client log in

--- PizzaBox/PizzaBox.Domain/Models/Order/Order.cs
+++ PizzaBox/PizzaBox.Domain/Models/Order/Order.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-    
-
 
 namespace PizzaBox.Domain.Models
 {
-
-    // public class Pizza : PizzaBox.Domain.Interfaces.IPizza
-    public class Pizza
+    class Order
     {
-
     }
 }
---
+++ PizzaBox/PizzaBox.Domain/Models/Pizza/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+    
+
+
+namespace PizzaBox.Domain.Models
+{
+
+    /// <summary>
+    /// have a crust
+    /// have a size
+    /// compute its cost
+    /// has minimum of 2 default toppings, including chees and sauce
+    /// limit toppings to no more than 5
+    /// </summary>
+    public class Pizza
+    {
+
+    }
+}
